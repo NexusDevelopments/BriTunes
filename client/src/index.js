842af27ad9ded8ae7faf2b79912eaa92b96f3a90
NexusDev <maurilewis564@gmail.com>
2026-01-10 20:11:34 +0000

    Fix React 18 entrypoint: use createRoot in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,15 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css'
-import {BrowserRouter as Router} from 'react-router-dom'
+import './index.css';
 
-
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
 
 
