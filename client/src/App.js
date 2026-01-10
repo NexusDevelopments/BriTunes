51d2864b61bbc2103f2a9b683b036fdad8885179
NexusDev <maurilewis564@gmail.com>
2026-01-10 19:26:36 +0000

    Restore working App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,53 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store/store';
-import { SpotifyPlayerProvider } from './context/SpotifyPlayerContext';
-
-// Components
-import MainLayout from './components/MainLayout/MainLayout';
-import TopNav from './components/TopNav/TopNav';
-import PrivateRoute from './components/PrivateRoute';
-
-// Pages
+import { AuthProvider } from './context/AuthContext';
+import { PlayerProvider } from './context/PlayerContext';
+import Navbar from './components/Navbar';
+import MusicPlayer from './components/MusicPlayer';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import HomePage from './pages/HomePage/HomePage';
-import SearchPage from './pages/SearchPage/SearchPage';
-import Browse from './pages/Browse/Browse';
-import AlbumDetail from './pages/AlbumDetail/AlbumDetail';
-import ArtistDetail from './pages/ArtistDetail/ArtistDetail';
-import PlaylistDetail from './pages/PlaylistDetail/PlaylistDetail';
+import Search from './pages/Search';
+import Artist from './pages/Artist';
+import Album from './pages/Album';
 import Library from './pages/Library';
 import Profile from './pages/Profile';
-
-// Services
-import spotifyAPI from './services/spotifyAPI';
-
+import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    // Set token for Spotify API from Redux store
-    const token = localStorage.getItem('spotify_token');
-    if (token) {
-      spotifyAPI.setToken(token);
-    }
-  }, []);
-
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <SpotifyPlayerProvider>
-          <Router>
-            <div className="app">
+    <AuthProvider>
+      <PlayerProvider>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="app-content">
               <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                
-                <Route
-                  path="/*"
-                  element={
-                    <PrivateRoute>
-                      <MainLayout>
-                        <TopNav />
-                        <Routes>
-                          <Route path="/" element={<HomePage />} />
-                          <Route path="/search" element={<SearchPage />} />
-                          <Route path="/browse" element={<Browse />} />
-                          <Route path="/library" element={<Library />} />
-                          <Route path="/profile" element={<Profile />} />
-                          <Route path="/album/:id" element={<AlbumDetail />} />
-                          <Route path="/artist/:id" element={<ArtistDetail />} />
-                          <Route path="/playlist/:id" element={<PlaylistDetail />} />
-                          <Route path="*" element={<Navigate to="/" replace />} />
-                        </Routes>
-                      </MainLayout>
-                    </PrivateRoute>
-                  }
-                />
+                <Route path="/" element={<Home />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/artist/:id" element={<Artist />} />
+                <Route path="/album/:id" element={<Album />} />
+                <Route path="/library" element={
+                  <PrivateRoute>
+                    <Library />
+                  </PrivateRoute>
+                } />
+                <Route path="/profile" element={
+                  <PrivateRoute>
+                    <Profile />
+                  </PrivateRoute>
+                } />
+                <Route path="*" element={<Navigate to="/" />} />
               </Routes>
             </div>
-          </Router>
-        </SpotifyPlayerProvider>
-      </PersistGate>
-    </Provider>
+            <MusicPlayer />
+          </div>
+        </Router>
+      </PlayerProvider>
+    </AuthProvider>
   );
 }
 
